dd09b8f19da4a4f6d9849cb98a857484dbf990c3
Hoss Agina <hoss.agina@telus.com>
2022-12-15 20:19:39 -0700

    Create a listing is now connecting to the database and adding properties

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -26,6 +26,7 @@ module.exports = function(router, database) {
 
   router.post('/properties', (req, res) => {
     const userId = req.session.userId;
+    console.log({...req.body, owner_id: userId});
     database.addProperty({...req.body, owner_id: userId})
       .then(property => {
         res.send(property);
